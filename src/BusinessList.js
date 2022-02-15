5cd02ee0c4dfef210ba5627f32982b2964c06b7a
TrevorRezac <trev.rezac@gmail.com>
2022-02-15 13:58:35 -0800

    yelp search form updates state on change

---
+++ src/BusinessList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Business from './Business';
+
+export default function BusinessList() {
+  return (
+    <div>BusinessList</div>
+  );
+}
--- src/YelpSearch.js
+++ src/YelpSearch.js
@@ -1,26 +1,39 @@
 import { useState } from 'react';
+import Spinner from './Spinner';
+import BusinessList from './BusinessList';
 
 export default function YelpSearch() {
     // you'll need to track your yelp search results, the loading state, and a form field for location with a default value.
+  const [results, setResults] = useState([]);
+  const [search, setSearch] = useState('portalnd or usa');
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleYelpSubmit(e) {
     e.preventDefault();
   
     // set the loading state to true
+    setIsLoading(true);
     // use fetch to make a request to your netlify yelp function. Be sure to pass the search query as a query param in the URL
   
     // put the jsonified data in state and set the loading state to false
+
+    setIsLoading(false);
   }
-  
+  console.log('||', search);
   return (
     <section className='yelp'>
       {/* make the fetch on submit */}
       <form>
         Search yelp for a city
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
+        <input 
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
         <button>Search yelp</button>
       </form>
       {/* Make a BusinessesList component to import and use here. Use a ternery to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
+      {isLoading ? <Spinner /> : <BusinessList />}
     </section>
   );
 }
